a6f7a0985eb6ec98b44c6258b2e28b50b46e57f2
Ismael Fernandez <98668201+ifernan7@users.noreply.github.com>
2023-09-18 00:08:08 -0500

    preparing for milestone 2
    adding docker-compose and flyway preparation code

--- src/main/java/com/online_ordering/presentation/Application.java
+++ src/main/java/com/online_ordering/presentation/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 
 @SpringBootApplication
-
 //note: This shows me real exceptions!
 @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class} )
 public class Application {
