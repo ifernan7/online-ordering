16ac6ef482881c08127aabce80284377068bcae3
Ismael Fernandez <98668201+ifernan7@users.noreply.github.com>
2023-09-19 23:30:51 -0500

    Update R__Seed.sql

--- src/main/resources/db/migration/R__Seed.sql
+++ src/main/resources/db/migration/R__Seed.sql
@@ -2,7 +2,7 @@
 
 TRUNCATE TABLE online_ordering_db.users;
 
-INSERT INTO users (email, password) VALUES
+INSERT INTO online_ordering_db.users (email, password) VALUES
     ('user1@example.com', 'password1'),
     ('user2@example.com', 'password2'),
     ('user3@example.com', 'password3');
