19f3a568d69712c93fa7cc169b8c7f37401187a5
Ismael Fernandez <98668201+ifernan7@users.noreply.github.com>
2023-09-21 10:18:25 -0500

    adding user table queries into repo layer

--- src/main/java/com/online_ordering/repository/IUserRepository.java
+++ src/main/java/com/online_ordering/repository/IUserRepository.java
@@ -2,8 +2,12 @@ package com.online_ordering.repository;
 
 import com.online_ordering.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Repository
@@ -12,5 +16,21 @@ public interface IUserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT id, email FROM online_ordering_db.users", nativeQuery = true)
     List<User> GetAllUsers();
 
-    //GetUserOrders1
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO online_ordering_db.users (email, password) VALUES (:email, :password)", nativeQuery = true)
+    void AddUser(@Param("email") String email, @Param("password") String password);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE online_ordering_db.users SET email = :email WHERE id = :id", nativeQuery = true)
+    void UpdateUserEmailById(@Param("id") int id, @Param("email") String email);
+
+    @Query(value = "SELECT id, email FROM online_ordering_db.users WHERE id = :id", nativeQuery = true)
+    List<User> GetUserById(@Param("id") int id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM online_ordering_db.users WHERE id = :id", nativeQuery = true)
+    void DeleteUserById(@Param("id") int id);
 }
