0379f49b4f376c77da056629b47d751176c7c325
Dilip <144294237+dilipsaireddy1059@users.noreply.github.com>
2023-09-23 11:53:37 -0500

    Help
    adding ordering classes

---
+++ src/main/java/com/online_ordering/order/OrderService.java
@@ -0,0 +1,4 @@
+package com.online_ordering.order;
+
+public class OrderService {
+}
--- src/main/java/com/online_ordering/order/abstractions/IOrderRepository.java
+++ src/main/java/com/online_ordering/order/abstractions/IOrderRepository.java
@@ -1,4 +1,15 @@
 package com.online_ordering.order.abstractions;
 
-public interface IOrderRepository {
+import com.online_ordering.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IOrderRepository extends JpaRepository<Order, Long> {
+
+    @Query(value = "SELECT id, user_id, order_date, status_id FROM online_ordering_db.orders", nativeQuery = true)
+    List<Order> GetAllOrders();
 }
