c0dde75a5f46a75d65b3ee353fc853f50a532a41
Dilip <144294237+dilipsaireddy1059@users.noreply.github.com>
2023-09-23 10:30:29 -0500

    Update Order.java

--- src/main/java/com/online_ordering/order/Order.java
+++ src/main/java/com/online_ordering/order/Order.java
@@ -1,4 +1,20 @@
 package com.online_ordering.order;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
 public class Order {
+    @Id
+    private int id;
+
+    private int user_id;
+
+    private Date order_date;
+
+    private int status_id;
 }
