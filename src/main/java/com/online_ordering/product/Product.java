9074a21e6de6c7381b86dacddeec8bdaf43e0dae
Ismael Fernandez <98668201+ifernan7@users.noreply.github.com>
2023-09-24 09:41:37 -0500

    fixing runtime issue with Products

--- src/main/java/com/online_ordering/product/Product.java
+++ src/main/java/com/online_ordering/product/Product.java
@@ -1,10 +1,13 @@
 package com.online_ordering.product;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Product {
+    @Id
     private int id;
     private String stockNumber;
     private String name;
