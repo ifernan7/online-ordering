0e6c13851219144a8c8bcfca3290751d8a71a6c5
Ismael Fernandez <98668201+ifernan7@users.noreply.github.com>
2023-10-28 13:31:12 -0500

    Create package-info.java
    for warehouse domain

---
+++ src/main/java/com/online_ordering/warehouse/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package is for processing warehouse data.
+ * <p>
+ * This package has functionality to get all warehouses and to create a new warehouse. From the persistence layer to do database inserts and retrievals to the service layer to perform additional processing on warehouses if necessary to even the web controllers to get requets and to return html pages this package has it all.
+ * </p>
+ *
+ * @since 1.0
+ * @author Ismael Fernandez
+ * @version 1.1
+ */
+package com.online_ordering.warehouse;
