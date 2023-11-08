01252c4070e7a2ae2e45f1f4a71bec1a71779da9
cBass <sebi.krawiec@gmail.com>
2023-11-07 21:27:53 -0600

    Added "DISTINCT" to SQL Query
    Added to prevent duplicates from being shown when a product is added to an order (still need to fix/potentially add a new call to get products from an order?)

--- src/main/java/com/online_ordering/product/abstractions/IProductRepository.java
+++ src/main/java/com/online_ordering/product/abstractions/IProductRepository.java
@@ -17,7 +17,7 @@ public interface IProductRepository extends JpaRepository<Product, Long> {
     //SELECT p.id ,p.name ,p.description ,p.price ,CASE WHEN op.id IS NULL THEN 0 ELSE 1 END AS on_order FROM online_ordering_db.products p LEFT JOIN online_ordering_db.order_products op ON p.id = op.product_id
     //@Query(value = "SELECT id, name, description, price FROM online_ordering_db.products", nativeQuery = true)
 
-    @Query(value = "SELECT p.id ,p.name ,p.description ,p.price ,CASE WHEN op.id IS NULL THEN 0 ELSE 1 END AS on_order FROM online_ordering_db.products p LEFT JOIN online_ordering_db.order_products op ON p.id = op.product_id", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT p.id ,p.name ,p.description ,p.price ,CASE WHEN op.id IS NULL THEN 0 ELSE 1 END AS on_order FROM online_ordering_db.products p LEFT JOIN online_ordering_db.order_products op ON p.id = op.product_id", nativeQuery = true)
     List<Product> GetAllProducts();
 
     @Transactional
