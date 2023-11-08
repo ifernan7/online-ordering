faa5ec453be3f452c68fb696a2c8600fcaeaa48f
Ismael Fernandez <98668201+ifernan7@users.noreply.github.com>
2023-11-07 19:23:06 -0600

    trying to process products that are already in the order

--- src/main/java/com/online_ordering/product/ProductAPIController.java
+++ src/main/java/com/online_ordering/product/ProductAPIController.java
@@ -7,6 +7,7 @@ import com.online_ordering.product.abstractions.IProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +23,8 @@ public class ProductAPIController {
         this._productService = productService;
     }
 
-    @GetMapping("/products")
-    public ResponseEntity<Object> GetAllProducts() {
+    @GetMapping("/products/{orderId}")
+    public ResponseEntity<Object> GetAllProducts(@PathVariable int orderId) {
         List<Product> products = _productService.GetAllProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
